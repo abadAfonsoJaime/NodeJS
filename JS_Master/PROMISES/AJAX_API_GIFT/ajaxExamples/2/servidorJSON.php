f7cadeb28054b0590ee15229369fb934e1c8dd30
Jaime Abad <jaime.abad.afonso@gmail.com>
2019-04-01 19:47:08 +0200

    moved JS to its own repo

--- JS_Master/PROMISES/AJAX_API_GIFT/ajaxExamples/2/servidorJSON.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//Recuperación del argumento pasado por la aplicación cliente
-	$codigo = $_POST("codigo");
-
-	//Definición de la consulta a enviar a la base de datos MySQL
-	$sql_query = "SELECT car_code, car_name, max_speed FROM cars WHERE car_code='$codigo';";
-?>
